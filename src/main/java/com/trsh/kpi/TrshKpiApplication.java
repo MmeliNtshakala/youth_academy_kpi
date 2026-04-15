1c54b9a4f80b68fe2539ae3a20d0ce973a535dca
Mmeli Ntshakala <mmeliroot@gmail.com>
2026-04-15 13:29:07 +0200

    spring boot: spring boot application and start of the program throgh the file trsh kpi application

--- src/main/java/com/trsh/kpi/TrshKpiApplication.java
+++ src/main/java/com/trsh/kpi/TrshKpiApplication.java
@@ -1,5 +1,12 @@
 package com.trsh.kpi;
+import org.springframework.boot.autoconfigure.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class TrshKpiApplication {
+
+    public static void main(String[] args){
+        SpringBootApplication.run(TrshKpiApplication.class, args)
+    }
     
 }
